a5457ecbd67756c7874cfece7c4b4ebda012c3f2
Azure DevOps Build Service <azure-devops@cataclysma.com>
2025-10-24 22:46:51 +0000

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,4 @@
+"""Async Python client for Sequence financial orchestration platform API.
+
+Provides SequenceApiClient and related exceptions for use in Home Assistant and other Python apps.
+"""
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for sequence_api_client package."""
